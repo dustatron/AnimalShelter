361736b105974bba460796b429462eb97693893e
Dusty <dustymccord@gmail.com>
2020-04-03 08:52:02 -0700

    add a README and Swagger.

--- ApiAnimalShelter/Startup.cs
+++ ApiAnimalShelter/Startup.cs
@@ -13,36 +13,56 @@ using Microsoft.Extensions.Options;
 
 namespace ApiAnimalShelter
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseMvc();
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+      services.AddSwaggerDocument(config =>
+      {
+        config.PostProcess = document =>
+        {
+          document.Info.Version = "v1";
+          document.Info.Title = "Animal Shelter API";
+          document.Info.Description = "A simple ASP.NET Core web API";
+          document.Info.TermsOfService = "None";
+          document.Info.Contact = new NSwag.OpenApiContact
+          {
+            Name = "Dusty McCord",
+            Email = string.Empty,
+            Url = "https://localhost:5000"
+          };
+
+        };
+      });
+
+
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+      else
+      {
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+      }
+      // app.UseHttpsRedirection();
+      app.UseOpenApi();
+      app.UseSwaggerUi3();
+      app.UseMvc();
+    }
+  }
 }
