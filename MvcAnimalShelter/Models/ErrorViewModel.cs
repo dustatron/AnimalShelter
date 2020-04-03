9d3d886273dc69e3a10d18e4beca62afb893b491
Dusty <dustymccord@gmail.com>
2020-04-03 12:56:01 -0700

    add mvc project.

---
+++ MvcAnimalShelter/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MvcAnimalShelter.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
