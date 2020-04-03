b1b7d37c03eb974ab80ca1a9e051c26cb031154c
Dusty <dustymccord@gmail.com>
2020-04-03 08:26:03 -0700

    inital commit.

---
+++ ApiAnimalShelter/Controllers/AnimalsController.cs
@@ -0,0 +1,8 @@
+using ApiAnimalShelter.Models;
+namespace ApiAnimalShelter.Controllers
+{
+  public class AnimalsController
+  {
+
+  }
+}
---
+++ ApiAnimalShelter/Models/Animal.cs
@@ -0,0 +1,7 @@
+namespace ApiAnimalShelter.Models
+{
+    public class Animal
+    {
+        
+    }
+}
