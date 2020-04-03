1aa6fb304892a9286aeb8c9fadbd9d6c4dcab39f
Dusty <dustymccord@gmail.com>
2020-04-03 10:05:17 -0700

    add basic crud methods to Animal Controller.

--- ApiAnimalShelter/Controllers/AnimalsController.cs
+++ ApiAnimalShelter/Controllers/AnimalsController.cs
@@ -1,8 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using ApiAnimalShelter.Models;
 namespace ApiAnimalShelter.Controllers
 {
-  public class AnimalsController
+  [Route("api/[controller]")]
+  [ApiController]
+  public class AnimalsController : ControllerBase
   {
+    private AnimalShelterContext _db;
 
+    AnimalsController(AnimalShelterContext db)
+    {
+      _db = db;
+    }
+
+    // GET api/values
+    [HttpGet]
+    public ActionResult<IEnumerable<Animal>> Get()
+    {
+      List<Animal> allAnimals = _db.Animals.ToList();
+      return allAnimals;
+    }
+
+    // GET api/values/5
+    [HttpGet("{id}")]
+    public ActionResult<string> Get(int id)
+    {
+      return "value";
+    }
+
+    // POST api/values
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+    }
+
+    // PUT api/values/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    // DELETE api/values/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
   }
 }
